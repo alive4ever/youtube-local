a69dc765f1805e33000db1abd1e711b5d55d1358
alive4ever <alive4ever@users.noreply.github.com>
2025-05-09 03:58:23 +0000

    util: ensure data_dir exist when handling cookies
    Ensure settings.data_dir exists before loading and saving cookies.

--- youtube/util.py
+++ youtube/util.py
@@ -228,6 +228,8 @@ def save_cookies():
     if not settings.use_httpx:
         pass
     else:
+        if not os.path.isdir(settings.data_dir):
+            os.makedirs(settings.data_dir)
         try:
             cookiejar.save(filename=cookiejar_file)
         except Exception:
@@ -237,6 +239,8 @@ def load_cookies():
     if not settings.use_httpx:
         pass
     else:
+        if not os.path.isdir(settings.data_dir):
+            os.makedirs(settings.data_dir)
         try:
             cookiejar.load(filename=cookiejar_file)
         except Exception:
