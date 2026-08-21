e8a0a35cf2a541304e8892a1707075a066c912ec
alive4ever <alive4ever@users.noreply.github.com>
2026-08-21 15:48:39 +0000

    util: Add visionos client
    Add visionos client, thanks to yt-dlp teams.

--- youtube/util.py
+++ youtube/util.py
@@ -814,6 +814,21 @@ INNERTUBE_CLIENTS = {
         'INNERTUBE_CONTEXT_CLIENT_NAME': 28,
         'REQUIRE_JS_PLAYER': False,
     },
+    'visionos': {
+        'INNERTUBE_CONTEXT': {
+            'client': {
+                'clientName': 'VISIONOS',
+                'clientVersion': '1.02',
+                'deviceMake': 'Apple',
+                'deviceModel': 'RealityDevice17,1',
+                'userAgent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 15_7_3) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/26.0 Safari/605.1.15',
+                'osName': 'visionOS',
+                'osVersion': '26.5.23O471',
+            },
+        },
+        'INNERTUBE_CONTEXT_CLIENT_NAME': 101,
+        'REQUIRE_JS_PLAYER': False,
+    },
 }
 
 innertube_client = list(INNERTUBE_CLIENTS.keys())
